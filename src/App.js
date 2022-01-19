4e25fbf37265eb9cdefb8b19f1f3b29b6dd1253d
Alexandre Morais <alexandremorais26@gmail.com>
2022-01-19 20:36:32 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import './App.css';
 import Company from './pages/Company';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import NewProject from './pages/NewProject';
+import Container from './components/layout/Container'
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste para o Commit</h1>
       <Router>
         <ul>
-          <li>Home</li>
-          <li>Company</li>
-          <li>Contact</li>
-          <li>New Project</li>
+          <li> <Link to="/">Home</Link> </li>
+          <li><Link to="/company">Company</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/newproject">New Project</Link></li>
         </ul>
         <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/company">
-          <Company/>
-        </Route>
-        <Route exact path="/contact">
-          <Contact/>
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject/>
-        </Route>
+          <Container customClass="min-height">
+            <Route exact path="/">
+               <Home/>
+            </Route>
+             <Route exact path="/company">
+              <Company/>
+            </Route>
+            <Route exact path="/contact">
+              <Contact/>
+            </Route>
+            <Route exact path="/newproject">
+              <NewProject/>
+            </Route>
+          </Container>
+        
       </Switch>
+      <p>Footer</p>
       </Router>
       
     </div>
