15eff05a77aa76edb36dfb17294e4240c7707a43
Alexandre Morais <alexandremorais26@gmail.com>
2022-01-25 18:43:37 -0300

    Sobrepondo commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import LinkButton from '../components/layout/LinkButton'
 function Home () {
   return (
     <section className={styles.home_container}>
-      <h1>Bem vindo ao <span>Costs</span></h1>
+      <h1>Bem vindo aooo <span>Costs</span></h1>
       <p>Comece a gerenciar seus projetos agora!</p>
       <LinkButton to="/newproject" text="Criar Projeto"/>
       
