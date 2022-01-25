5d7590ab1d829b153a7546e64ce73ee9dc471376
Alexandre Morais <alexandre.morais@gmail.com>
2022-01-25 14:34:37 -0300

    Atualizando

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-function ProjectForm () {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" placeholder="Insira o nome do projeto" />
-        </div>
-        <div>
-        <input type="number" placeholder="Insira o valor de orçamento do projeto" />
-        </div>
-        <div>
-          <select name="categoria">
-            <option disabled selected>Selecione a categoria</option>
-          </select>
-        </div>
-        <input type="submit" value="Criar projeto"/>
-      </form>
-    </div>
-  )
-=======
+
 import {useState} from 'react'
 
 import styles from './ProjectForm.module.css'
@@ -69,7 +49,6 @@ function ProjectForm ({btnText}) {
       
     </form>
   ) 
->>>>>>> c994a63cb441ef3d8e9c969c24f007145ebfa5ad
 }
 
 export default ProjectForm
