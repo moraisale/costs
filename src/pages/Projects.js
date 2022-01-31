949cf0410c6dda4fdf096774418a1dc1bf8a410b
Alexandre Morais <alexandremorais26@gmail.com>
2022-01-31 20:10:03 -0300

    Implementando mensagens e dashboard para a pág Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,30 @@
+import {useLocation} from 'react-router-dom'
+import styles from './Projects.module.css'
+
+import LinkButton from '../components/layout/LinkButton'
 import Message from '../components/layout/Message'
+import Container from '../components/layout/Container'
 
 function Projects () {
+
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
   return (
-    <div>
-      <h1>Meus projetos</h1>
-      <Message msg="Teste" type="error"/>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto"/>
+      </div>
+      {message && <Message type="success" msg={message}/>}
+      <Container customClass="start">
+        <p>Projetos</p>
+      </Container>
     </div>
+    
   )
 }
 export default Projects
